63c6b0c69a92ab03456be9d18e2908831fea7395
caspar wikstrom <caspar.wikstrom@gmail.com>
2023-04-11 14:08:43 +0200

    tttt

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,22 @@
-import Link from '@/components/Link'
-import { PageSEO } from '@/components/SEO'
-import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
-import formatDate from '@/lib/utils/formatDate'
-import NewsletterForm from '@/components/NewsletterForm'
-import getConfig from 'next/config'
-import Image from '../components/Image'
-import ListLayout from '@/layouts/ListLayout'
+import Link from '@/components/Link';
+import { PageSEO } from '@/components/SEO';
+import Tag from '@/components/Tag';
+import siteMetadata from '@/data/siteMetadata';
+import NewsletterForm from '@/components/NewsletterForm';
+import getConfig from 'next/config';
+import Image from '../components/Image';
+import ListLayout from '@/layouts/ListLayout';
 
+const { publicRuntimeConfig } = getConfig();
+const isDevelopment = publicRuntimeConfig.isDevelopment;
+//pagination with max 10 posts per page
 
-const { publicRuntimeConfig } = getConfig()
-const isDevelopment = publicRuntimeConfig.isDevelopment
+export const VIDEOS_PER_PAGE = 10;
 
-//pagination with max 3 posts per page
-
-export const VIDEOS_PER_PAGE = 10
 
 export async function getStaticProps() {
-  const v = await fetch(isDevelopment ? 'http://localhost:3001/api/v1/videos' : "https://you-b.herokuapp.com/api/v1/videos")
+  const domain = process.env.DOMAIN_URL
+  const v = await fetch(isDevelopment ? `http://localhost:3001/api/v1/videos?domain=${domain}` : `https://you-b.herokuapp.com/api/v1/videos?domain=${domain}`)
   const videos = await v.json()
 
   const initialDisplayVideos = videos.slice(0, VIDEOS_PER_PAGE)
@@ -49,3 +48,4 @@ export default function Home({ videos, initialDisplayVideos, pagination }) {
     </>
   )
 }
+
