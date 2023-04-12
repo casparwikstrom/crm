6c0ae891852b6b8fd528e03897a49361a01b9353
caspar wikstrom <caspar.wikstrom@gmail.com>
2023-04-12 09:41:36 +0200

    dasdasd

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: 'https://fastly.picsum.photos/id/583/200/200.jpg?hmac=6WdmRS6vtdQqa1zktgLfV0t3Afny4XE1zeDdlJ2K_mo',
   email: 'zmartare@yoursite.com',
   github: 'https://github.com',
   twitter: 'https://twitter.com/Twitter',
