f07657d5d6d4d5452afda0cf8e96e1ccc9da7b31
caspar wikstrom <caspar.wikstrom@gmail.com>
2023-09-28 09:22:39 +0200

    ddd

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -96,8 +96,9 @@ export default function ListLayout({ videos, title, initialDisplayVideos = [], p
                         ))}
                       </div>
                     </div>
-                  
-                    <div className="" dangerouslySetInnerHTML={{ __html: (singel_video?.description ? singel_video?.description.substr(0, 200) + '...' : "") }} />
+                    <div className="" >
+                      {(singel_video?.description ? singel_video?.description.substr(0, 200) + '...' : "")}
+                    </div>
                   </div>
                 </article>
               </li>
