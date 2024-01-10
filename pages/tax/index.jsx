7545d2c1140b176248702274b72c30db4324a37f
caspar wikstrom <caspar.wikstrom@gmail.com>
2024-01-10 15:50:18 +0100

    Add Tax page with TaxCalculator component

---
+++ pages/tax/index.jsx
@@ -0,0 +1,13 @@
+"use-client"
+import TaxCalculator from '@/components/tax/TaxCalculator';
+
+export default function Tax() {
+
+    return (
+      <div>
+        <TaxCalculator />
+      </div>
+    );
+}
+
+
